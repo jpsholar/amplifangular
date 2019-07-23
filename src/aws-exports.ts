ee809feadb44bc7a37404cef53704ea13c3c37c8
jpsholar <jpsholar@gmail.com>
2019-07-23 12:29:34 -0500

    updated client id

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -1,7 +1,7 @@
 const awsexports = {
     region: "us-east-1",
     userPoolId: "us-east-1_LRNKsQS2w",
-    userPoolWebClientI: "1p72eqe2qoabdrerv7vp80ejgd"
+    userPoolWebClientId: "1p72eqe2qoabdrerv7vp80ejgd"
 };
 
 export default awsexports;
