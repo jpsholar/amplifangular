168e3d161c882f06b7bf5d7cbf5873f3d586b0a3
jpsholar <jpsholar@gmail.com>
2019-07-23 09:15:04 -0500

    create external module

---
+++ src/app/external/external.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ExternalRoutingModule } from './external-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ExternalRoutingModule
+  ]
+})
+export class ExternalModule { }
