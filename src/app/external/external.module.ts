095be36d0bd98ffd33bb460ea32ac42696a68a7f
jpsholar <jpsholar@gmail.com>
2019-07-23 09:17:22 -0500

    create login component

--- src/app/external/external.module.ts
+++ src/app/external/external.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ExternalRoutingModule } from './external-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
     ExternalRoutingModule
