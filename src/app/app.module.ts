0b317db210a868926ccceda7851a86b43b8995d8
jpsholar <jpsholar@gmail.com>
2019-07-23 10:55:09 -0500

    prep for auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -12,7 +11,6 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/aws-exports.ts
@@ -0,0 +1,7 @@
+const awsexports = {
+    region: "us-east-1",
+    userPoolId: "us-east-1_LRNKsQS2w",
+    userPoolWebClientI: "1p72eqe2qoabdrerv7vp80ejgd"
+};
+
+export default awsexports;
