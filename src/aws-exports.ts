80551903d27cb7bcdea416fbc17e3bc9764121d7
jpsholar <jpsholar@gmail.com>
2019-07-26 17:32:46 -0500

    comment out aws config

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -1,7 +1,7 @@
 const awsexports = {
-    region: "us-east-1",
-    userPoolId: "us-east-1_LRNKsQS2w",
-    userPoolWebClientId: "1p72eqe2qoabdrerv7vp80ejgd"
+    // region: "",
+    // userPoolId: "",
+    // userPoolWebClientId: ""
 };
 
 export default awsexports;
